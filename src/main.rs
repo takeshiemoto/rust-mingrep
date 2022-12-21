3702b808549cdb0f99a5642bbe6e71e5ed0cd98a
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2022-12-22 06:51:15 +0900

    12.3 run function

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // POINT: unwrap_or_elseはOkならOkの値を、Errなら独自のエラー処理を定義できる。
+    // unwrap_or_elseはOkならOkの値を、Errなら独自のエラー処理を定義できる。
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
-        // POINT: process::exitはpanic!と違い余計な出力がない。
+        // process::exitはpanic!と違い余計な出力がない。
         process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
-
-    let mut content = String::new();
-    f.read_to_string(&mut content)
-        .expect("something went wrong reading the file");
-
-    println!("With text:\n{}", content);
+    // if letでErr値を返したかを確認する
+    // Ok値が不要なのでunwrapした値は不要。
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -29,7 +29,7 @@ struct Config {
 }
 
 impl Config {
-    // POINT: &'static strは文字列リテラルの型
+    // &'static strは文字列リテラルの型
     fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
@@ -39,4 +39,18 @@ impl Config {
 
         Ok(Config { query, filename })
     }
+}
+
+// ()はユニット型。空を表す。
+// dynはdynamicの略。エラーケースによって異なる型のError値を返すことができる。
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // ?演算子は呼び出し元にエラーを委譲する処理の省略記法。
+    let mut f = File::open(config.filename)?;
+
+    let mut content = String::new();
+    f.read_to_string(&mut content)?;
+
+    println!("With text:\n{}", content);
+
+    Ok(())
 }
